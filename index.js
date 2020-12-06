f93f2d37205b6a0fc529dcea845f7e4be123565d
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-06 16:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+function saturdayFun( activity = "roller-skate"){
+  return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork( act = "go to the office"){
+  return(`This Monday, I will ${act}.`);
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective( symbol = "*"){
+  return function(adjective="special"){
+    return(`You are ${symbol}${adjective}${symbol}!`);
+  };
+}
+wrapAdjective("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
+
+let encouragingPromptFunction = wrapAdjective("||");
+wrapAdjective("%")("a dedicated programmer");
+
+
+ let Calculator = {add : function(x,y){return x+y;} ,
+                   subtract: function(x,y){return x-y;} ,
+                   multiply: function(x,y){return x*y;} ,
+                   divide : function(x,y){return x/y;}
+                   };
+                   Calculator.add(1,3);
+                   calculator.subtract(1,3);
+                   calculator.divide(10,5);
+                   calculator.multiply(1,3);
+        
+function actionApplyer(int , arr){
+  let n = int
+  
+  for (let i=0; i<arr.length; i++)
+   {n = arr[i](n)}
+   return n
+}
+
+
+
+
+                   
+
